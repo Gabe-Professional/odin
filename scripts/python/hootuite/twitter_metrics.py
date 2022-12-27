811696658b594d45ecf1c10959f51b1d690a4570
Gabriel McBride <gabriel.mcbride@twosixtech.com>
2022-12-27 14:18:16 -0800

    adding ability to change variables

--- scripts/python/hootuite/twitter_metrics.py
+++ scripts/python/hootuite/twitter_metrics.py
@@ -3,8 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import re
 import os
-
-
+import scipy.stats as stats
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
+import seaborn as sns
 
 
 def get_reward_offer_amount(text):
@@ -48,12 +50,20 @@ def has_website(text):
         return 0
 
 
+def has_string(string: str, body: str):
+    if string in body:
+        return 1
+    else:
+        return 0
+
+
 def main():
     ###### SET THE VARIABLES ######
     pd.set_option('display.max_rows', None)
-    metric = 'engagements'
-    x = 'reward_amount'
-    # directory = os.path.join('~', 'output', 'odin', 'twitter_effectiveness')
+    metric = 'impressions'
+    string = '!'
+    xs = ['reward_amount', f'has_char']
+    variable = xs[1]
     directory = os.path.expanduser('~/output/odin/twitter_effectiveness')
     print(directory)
     filepath = '~/data/odin/social_media/hootsuite/rfj_metrics/all_time/twitter_post_performance_2020-01-01_to_2022-12-19_created_on_20221219T2006Z_twitter_post_performance.csv'
@@ -67,62 +77,96 @@ def main():
     df['reward_amount'] = df['post_message'].apply(lambda x: get_reward_offer_amount(x))
     df['reward_offer_post?'] = df['post_message'].apply(lambda x: is_reward_offer(x))
     df['has_website?'] = df['post_message'].apply(lambda x: has_website(x))
+    df[variable] = df['post_message'].apply(lambda x: has_string(string=string, body=x))
+
     N = len(df)
 
-
-    # df = df[df['reward_offer_post?'] == 1]
-
     df = df[df[metric] > 0]
-    n_var = len(df)
+    n_metric = len(df)
     data = {'log_impressions': np.log10(df['impressions']),
             'log_engagements': np.log10(df['engagements']),
             'reward_offer_post?': df['reward_offer_post?'],
-            'reward_amount': df['reward_amount']}
+            'reward_amount': df['reward_amount'],
+            variable: df[variable]}
     data_df = pd.DataFrame(data)
-    tmp = data_df[data_df[x] > 0]
-    n_pos_ro = len(tmp)
-    ro_amts = tmp[x].unique()
-    n_ro_amts = len(ro_amts)
+    # tmp = data_df[data_df[xs] > 0]
+    n_pos_ro = len(data_df)
+    variable_values = data_df[variable].unique()
+    n_categories = len(variable_values)
 
-    f, axs = plt.subplots(figsize=(10, 10), nrows=n_ro_amts)
+    f, axs = plt.subplots(figsize=(10, 10), nrows=n_categories)
 
-    for tup in enumerate(ro_amts):
+    for tup in enumerate(variable_values):
         i = tup[0]
-        n = len(tmp[tmp[x] == ro_amts[i]])
-        axs[i].hist(tmp[tmp[x] == ro_amts[i]][f'log_{metric}'], ec='w')
-        axs[i].set_title(f'RO Amount: {ro_amts[i]}, n: {n}')
+        n = len(data_df[data_df[variable] == variable_values[i]])
+        axs[i].hist(data_df[data_df[variable] == variable_values[i]][f'log_{metric}'], ec='w')
+        axs[i].set_title(f'{variable}: {variable_values[i]}, n: {n}')
 
     plt.tight_layout()
-    fp = os.path.join(directory, f'{metric}_{x}_histogram.png')
+    fp = os.path.join(directory, f'{metric}_{variable}_histogram.png')
     f.savefig(fp)
 
     print(f'Total initial data points: {N} \n'
-          f'Total data points {metric} > 0: {n_var} \n'
-          f'Unique reward offer amounts: {n_ro_amts} \n'
-          f'Data points with RO > 0: {n_pos_ro}')
-
+          f'Total data points {metric} > 0: {n_metric} \n'
+          f'Unique {variable} : {string}: {n_categories} \n'
+          f'Data points with valid {variable}: {n_pos_ro}')
     # todo: run multiple iterations with different sample sizes
 
     sample_size = 100
+    state = 1
+    # data_df['ro_over_0'] = data_df[xs].apply(lambda a: 1 if a > 0 else 0)
+    test_samples = []
     f, ax = plt.subplots(nrows=2, figsize=(10, 10))
-    tmp = data_df[data_df[x] > 0]
-    ax[0].hist(tmp[f'log_{metric}'], ec='w')
-    n_tmp = len(tmp)
-    x_bar = np.mean(tmp[f'log_{metric}']).round(decimals=3)
-    v = np.var(tmp[f'log_{metric}']).round(decimals=3)
-    ax[0].set_title(f'n > 0: {n_tmp}, mean: {x_bar}, var: {v}')
+    for i in enumerate(variable_values):
+        tmp = data_df[data_df[variable] == i[0]].sample(n=sample_size, random_state=state)
+        assert len(tmp) == sample_size
 
-
-    tmp = data_df[data_df[x] == 0]
-    ax[1].hist(tmp[f'log_{metric}'], ec='w')
-    n_tmp = len(tmp)
-    x_bar = np.mean(tmp[f'log_{metric}']).round(decimals=3)
-    v = np.var(tmp[f'log_{metric}']).round(decimals=3)
-    ax[1].set_title(f'n = 0: {n_tmp}, mean: {x_bar}, var: {v}')
-
+        test_samples.append(tmp)
+        ax[i[0]].hist(tmp[f'log_{metric}'], ec='w')
+        n_tmp = len(tmp)
+        x_bar = np.mean(tmp[f'log_{metric}']).round(decimals=3)
+        v = np.var(tmp[f'log_{metric}']).round(decimals=3)
+        ax[i[0]].set_title(f'variable: {i[0]}, n: {n_tmp}, mean: {x_bar}, variance: {v}')
+        ax[i[0]].set_ylabel(metric)
     plt.tight_layout()
+    f.savefig(os.path.join(directory, f'{metric}_0vsRO_histogram.png'))
+    test_sample_df = pd.concat(test_samples)
+    assert len(test_sample_df) == n_categories * sample_size
 
-    f.savefig(os.path.join(directory, '0vsRO_histogram.png'))
+    f, ax = plt.subplots(figsize=(10, 10))
+    ax = sns.boxplot(x=f'{variable}', y=f'log_{metric}', data=test_sample_df[[f'{variable}', f'log_{metric}']], color='#99c2a2')
+    # ax = sns.swarmplot(x=f'ro_over_0', y=f'log_{metric}', data=data_df[['ro_over_0', f'log_{metric}']], color='#7d0013')
+    model = ols(f'log_{metric} ~ C(has_char)', data=test_sample_df[[f'{variable}', f'log_{metric}']]).fit()
+    anova_table = sm.stats.anova_lm(model, typ=2)
+    # print(list(anova_table.index))
+    # print(list(anova_table.columns))
+    # print(np.asarray(anova_table))
+    # exit()
+
+    rows = []
+    for r in range(len(anova_table)):
+        row = list(anova_table.iloc[r])
+        rows.append(row)
+
+    row_labels = list(anova_table.index)
+    col_labels = list(anova_table.columns)
+    # print(rows, row_labels, col_labels)
+    # exit()
+
+    table = plt.table(cellText=rows,
+                      colWidths=[0.2] * 4,
+                      rowLabels=row_labels,
+                      colLabels=col_labels,
+                      loc='top')
+
+    print(anova_table)
+    print(type(anova_table))
+    # plt.text('Anova Table', size=12)
+    plt.tight_layout()
+    f.savefig(os.path.join(directory, f'{metric}_{variable}_boxplot.png'))
+
+
+
 
 
 if __name__ == '__main__':
