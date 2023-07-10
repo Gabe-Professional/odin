b0b60d530ad2711dc60c0e7c7aa5727dab8b955b
Gabriel McBride <gabriel.mcbride@twosixtech.com>
2023-07-10 12:41:48 -0700

    making cleanup changes.

--- tests/vis/test_google_analytics.py
+++ tests/vis/test_google_analytics.py
@@ -8,4 +8,4 @@ def test_plot_activity():
     dates = pd.date_range(start='2023-06-14', end='2023-06-20', freq='d').tolist()
     views = [20, 209, 68, 148, 154, 126, 171]
     data = GData(x=dates, y=views, country=country)
-    data.plot_activity(show=True)
+    data.plot_activity(show=False)
